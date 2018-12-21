00cbd6d75f4f1483e428888fec49ed7377ff2cb1
DanielDabrowski <45854328+DanielDabrowski@users.noreply.github.com>
2018-12-21 21:11:32 +0100

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
